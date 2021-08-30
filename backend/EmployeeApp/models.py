04b71d3fd7a01f5238388c22a1fffbda2575f12c
ThisuraThejith <tphilips@wiley.com>
2021-08-31 00:01:54 +0530

    Completed backend

---
+++ backend/EmployeeApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db.models.base import Model
+
+# Create your models here.
+
+class Departments(models.Model):
+    DepartmentId = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=500)
+
+class Employees(models.Model):
+    EmployeeId = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=500)
+    Department = models.CharField(max_length=500)
+    DateOfJoining = models.DateField()
+    PhotoFileName = models.CharField(max_length=500)
